ba21e39a34714f0c7a12ea8db5ec21aaea4dd603
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2024-04-25 15:39:33 +0200

    Update MotionDetect.ino

--- MotionDetect.ino
+++ MotionDetect.ino
@@ -1,8 +1,10 @@
 // Definícia pinov
-const int pirPin = 2; // PIR senzor pripojený na pin 2
-const int led1Pin = 3; // Prvá LED pripojená na pin 3
-const int led2Pin = 4; // Druhá LED pripojená na pin 4
-const int led3Pin = 5; // Tretia LED pripojená na pin 5
+const int pirPin = 2;     // PIR senzor pripojený na pin 2
+const int led1Pin = 3;    // Prvá LED pripojená na pin 3
+const int led2Pin = 4;    // Druhá LED pripojená na pin 4
+const int led3Pin = 5;    // Tretia LED pripojená na pin 5
+const int buzzerPin = 6;  // Bzučiak pripojený na pin 6
+
 
 // ----------------------------------------
 // PIR --> Arduino Nano
@@ -13,14 +15,20 @@ const int led3Pin = 5; // Tretia LED pripojená na pin 5
 // LED Diodes --> Arduino Nano
 // D3, D4, D5
 // GND --> GND
+// BUZZER (optional for use)
+// BuzzerPin D6
 // ----------------------------------------
 
+
+
 // Premenné pre sledovanie pohybu
-bool motionDetected = false; // Príznak detekcie pohybu
+bool motionDetected = false;       // Príznak detekcie pohybu
 unsigned long motionStartTime = 0; // Čas od detekcie pohybu
 
 // Doba trvania LED
 const unsigned long ledDuration = 15000; // 15 sekúnd
+// Doba trvania zvuku bzučiaka
+const unsigned long buzzerDuration = 100; // 0,1 sekundy
 
 void setup() {
   // Nastavenie pinov
@@ -28,6 +36,7 @@ void setup() {
   pinMode(led1Pin, OUTPUT);
   pinMode(led2Pin, OUTPUT);
   pinMode(led3Pin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
 
   // Zhasnutie LED
   digitalWrite(led1Pin, LOW);
@@ -49,6 +58,8 @@ void loop() {
     digitalWrite(led1Pin, HIGH);
     digitalWrite(led2Pin, HIGH);
     digitalWrite(led3Pin, HIGH);
+    // Zapni bzučiak
+    tone(buzzerPin, 1000, buzzerDuration);
   }
 
   // Ak uplynula doba trvania LED a pohyb bol detekovaný
@@ -59,5 +70,7 @@ void loop() {
     digitalWrite(led1Pin, LOW);
     digitalWrite(led2Pin, LOW);
     digitalWrite(led3Pin, LOW);
+    // Vypni bzučiak
+    noTone(buzzerPin);
   }
 }
